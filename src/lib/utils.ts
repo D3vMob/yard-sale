c7c2ae3f7f01549b9c1c013b12f24227ccb55138
adesbiens <adesbiens>
2025-01-22 11:46:29 +0900

    fixed date issue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,12 +7,14 @@ export function cn(...inputs: ClassValue[]) {
 
 export function calculateDiscountedPrice(price: number, sold: boolean) {
   const currentDate = new Date();
-  const jan30 = new Date("2024-01-31");
+  const jan30 = new Date('2025-01-31');
+
 
   // If current date is after Jan 30
   if (currentDate > jan30 && !sold) {
     return Math.floor(price * 0.3); // 60% of original price
-  } else {
+  } 
+  else {
     return Math.floor(price * 0.6); // 30% of original price
   }
 }
